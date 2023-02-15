c51a108adde0aab587204efb3b99efd7dfee10fd
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-15 01:34:11 -0500

    update seed file with async await function

--- seeders/20230215062949-drone.js
+++ seeders/20230215062949-drone.js
@@ -3,23 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Drones', [{
+      name: 'Air 2S',
+      brand: 'DJI',
+      releaseYear: '2021',
+      createdAt: date,
+      updatedAt: date,
+    }])
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+    await queryInterface.bulkDelete('Drones', null, {})
+
   }
 };
