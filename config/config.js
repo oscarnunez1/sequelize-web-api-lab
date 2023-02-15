07b97bb2b0741a0a4d114bdb4fa03edbf3686b2b
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-15 01:04:12 -0500

    update code in config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,21 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config()
+
+const options = { ssl: { rejectUnauthorized: false, require: true } }
+
+module.exports = {
+  development: {
+    dialect: "postgres",
+    dialectOptions: options,
+    use_env_variable: "DATABASE_URL",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    dialect: "postgres",
+    dialectOptions: options,
+    use_env_variable: "DATABASE_URL",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    dialect: "postgres",
+    dialectOptions: options,
+    use_env_variable: "DATABASE_URL",
   }
 }
