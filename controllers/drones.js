2b0d7a3231d34a07cf900cd80c294a36f38e7a3a
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-16 08:09:52 -0500

    build create controller

--- controllers/drones.js
+++ controllers/drones.js
@@ -1,5 +1,14 @@
 const { Drone } = require("../models")
 
+const create = async (req, res) => {
+  try {
+    const cat = await Cat.create(req.body)
+    res.status(200).json(cat)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
-  
+  create,
 }
