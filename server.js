b29d5d9b424d6f4889fa4a14008ff9998c167e8c
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-16 08:02:59 -0500

    import dronesRouter and mount it

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 // import routers
 const indexRouter = require('./routes/index.js')
+const dronesRouter = require('./routes/drones.js')
 
 // create the express app
 const app = express()
@@ -17,6 +18,7 @@ app.use(express.json())
 
 // mount imported routes
 app.use('/', indexRouter)
+app.use('/api/drones', dronesRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
