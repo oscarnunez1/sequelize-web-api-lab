5e57d4ca98275623346938c222615dda77afc7ea
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-16 08:22:09 -0500

    fix incorrect variable and model in create controller

--- controllers/drones.js
+++ controllers/drones.js
@@ -2,9 +2,10 @@ const { Drone } = require("../models")
 
 const create = async (req, res) => {
   try {
-    const cat = await Cat.create(req.body)
+    const drone = await Drone.create(req.body)
     res.status(200).json(cat)
   } catch (error) {
+    console.log(error)
     res.status(500).json(error)
   }
 }
