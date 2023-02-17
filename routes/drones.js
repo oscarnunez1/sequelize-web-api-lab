9f67393d2890cc28ebad34f16f4e1f5faf7b3dc1
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-17 00:27:17 -0500

    create route for addAccessory

--- routes/drones.js
+++ routes/drones.js
@@ -6,5 +6,6 @@ router.post('/', dronesCtrl.create)
 router.get('/', dronesCtrl.index)
 router.put('/:id', dronesCtrl.update)
 router.delete('/:id', dronesCtrl.delete)
+router.post('/:id/accessories', dronesCtrl.addAccessory)
 
 module.exports = router
